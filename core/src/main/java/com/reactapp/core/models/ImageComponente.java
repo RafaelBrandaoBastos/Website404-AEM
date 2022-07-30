f7ca44602ff222ecb87ce88316e41493451bbe7f
Rafael Brandão <rafaelbrandaobastos@gmail.com>
2022-07-30 01:30:39 -0300

    Fix: Fixing Responsivity

---
+++ core/src/main/java/com/reactapp/core/models/ImageComponente.java
@@ -0,0 +1,40 @@
+/*
+ * ***********************************************************************
+ * React App CONFIDENTIAL
+ * ___________________
+ *
+ * Copyright 2022 React App.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property
+ * of React App and its suppliers, if any. The intellectual and
+ * technical concepts contained herein are proprietary to React App
+ * and its suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from React App.
+ * ***********************************************************************
+ */
+
+package com.reactapp.core.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.wcm.core.components.models.Image;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.osgi.annotation.versioning.ConsumerType;
+
+
+/**
+ * Defines the {@code ImageComponente} Sling Model used for the {@code reactapp/components/image-componente} component.
+ * 
+ */
+@ConsumerType
+public interface ImageComponente
+    extends ComponentExporter
+{
+
+
+    @JsonProperty("image")
+    Image getImage();
+
+}
